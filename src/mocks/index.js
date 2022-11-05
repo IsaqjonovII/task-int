d7177ed649787a0601a93cc871d0909c655fc162
ilhomjon003 <ilhomjonisoqjonov2@gmail.com>
2022-11-05 13:45:33 +0500

    ready

--- src/mocks/index.js
+++ src/mocks/index.js
@@ -26,5 +26,32 @@ export const PRODUCTS__DATA = [
     price: 999,
     dividedpayment: 333,
     label: [<ChangeIcon />],
-  }
+  },
+  {
+    id: 3,
+    title: "iPhone 14 pro max",
+    image:
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-finish-select-202209-6-7inch-spaceblack?wid=5120&hei=2880&fmt=jpeg&qlt=90&.v=1663703841897",
+    price: 1299,
+    dividedpayment: 333,
+    label: [<GiftIcon />, <ChangeIcon />],
+  },
+  {
+    id: 4,
+    title: "Samsung Galaxy Z flip",
+    image:
+      "https://images.samsung.com/uk/smartphones/galaxy-z-flip3-5g/buy/02_Carousel/03-BasicColor/zflip3_carousel_foldunfoldcombo_lavender_mo.jpg",
+    price: 600,
+    dividedpayment: 333,
+    label: [<ChangeIcon />],
+  },
+  {
+    id: 5,
+    title: "iPhone 13 pro max",
+    image:
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-finish-select-202209-6-7inch-spaceblack?wid=5120&hei=2880&fmt=jpeg&qlt=90&.v=1663703841897",
+    price: 1090,
+    dividedpayment: 333,
+    label: [<GiftIcon />, <ChangeIcon />],
+  },
 ];
